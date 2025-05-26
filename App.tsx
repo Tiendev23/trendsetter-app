c351fe4be15b2ad9366a8f1cd98cc025972b4a41
Tiến Trịnh <tientq.dev.it@gmail.com>
2025-05-26 22:48:23 +0700

    feat: Giao diện Account

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './src/navigation/AppNavigator';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Account } from './src/screens/tabs';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+    // <NavigationContainer>
+    //   <AppNavigator />
+    // </NavigationContainer>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Account />
+    </SafeAreaView>
   );
 }
