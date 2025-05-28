9a909779253d67799f8ceddfaea840c4768b180e
Tiến Trịnh <tientq.dev.it@gmail.com>
2025-05-28 23:09:38 +0700

    fix branch

--- App.tsx
+++ App.tsx
@@ -3,14 +3,17 @@ import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './src/navigation/AppNavigator';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Account } from './src/screens/tabs';
+import LoginScreen from './src/screens/LoginScreen';
+import AuthNavigator from './src/navigation/AuthNavigator';
+import { Text } from 'react-native';
 
 export default function App() {
   return (
     // <NavigationContainer>
-    //   <AppNavigator />
+    //   <AuthNavigator />
     // </NavigationContainer>
     <SafeAreaView style={{ flex: 1 }}>
-      <Account />
+      <LoginScreen />
     </SafeAreaView>
   );
 }
