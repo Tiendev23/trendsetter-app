9ef57a8595ac177323921ed9dd2d4033cdd4c6bd
taidev <taint.dev.it@gmail.com>
2025-05-30 18:09:00 +0700

    Lỗi

--- App.tsx
+++ App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './src/navigation/AppNavigator';
-import HomeScreen from './src/screens/HomeScreen';
-import { AppProvider } from './src/contexts/AuthContext';
 
 export default function App() {
   return (
-    <AppProvider>
-      <NavigationContainer>
-        <HomeScreen />
-      </NavigationContainer>
-    </AppProvider>
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
   );
 }
--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -15,7 +15,6 @@ export default function AppNavigator() {
       id={undefined}
       initialRouteName="Home"
       screenOptions={({ route }) => ({
-        headerShown: false,
         tabBarIcon: ({ focused, color, size }) => {
           let iconName = '';
 
@@ -35,7 +34,7 @@ export default function AppNavigator() {
         tabBarInactiveTintColor: 'gray',    // Màu khi tab không chọn
       })}
     >
-      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Home" component={HomeScreen} options={{ title: 'Trang chủ' }} />
       <Tab.Screen name="Search" component={SearchScreen} options={{ title: 'Tìm kiếm' }} />
       <Tab.Screen name="Information" component={InformationScreen} options={{ title: 'Thông tin' }} />
       <Tab.Screen name="Account" component={AccountScreen} options={{ title: 'Tài khoản' }} />
