641cc6eb047b880879a3b6eb9e14f0fdb0292ed1
Tiến Trịnh <tientq.dev.it@gmail.com>
2025-07-29 01:46:58 +0700

    Refactor type definitions for models and redux state
    Converted several type aliases to interfaces for better extensibility and consistency in cartItem, product, and user models. Enhanced cart item types to distinguish between payload, DB, and UI representations. Updated redux state types to improve error handling and response structure, introducing APIError, BaseResponse, AsyncState, and LoginResponse types.

--- src/types/models/cartItem.ts
+++ src/types/models/cartItem.ts
@@ -1,7 +1,36 @@
 import { ObjectId } from "../common";
 
-export type CartItem = {
-    variantId: ObjectId;
-    sizeId: ObjectId;
+export interface BaseItemProps {
     quantity: number;
-};
+}
+// Payload khi POST thêm/bớt
+export interface CartItemLite extends BaseItemProps {
+    sizeId: ObjectId;
+}
+
+// Data server trả về trực tiếp từ DB sau POST/PUT
+export interface CartItemDB extends BaseItemProps {
+    _id?: ObjectId;
+    user: ObjectId;
+    variantSize: ObjectId;
+    __v?: number;
+}
+
+// Data dùng để hiển thị lên front-end
+export interface CartItem extends BaseItemProps {
+    _id?: ObjectId;
+    product: ObjectId;
+    variant: ObjectId;
+    size: {
+        _id: ObjectId;
+        size: string;
+    };
+    campaign: ObjectId | null;
+    name: string;
+    color: string;
+    basePrice: number;
+    finalPrice: number;
+    imageUrl: string;
+}
+
+export type CartItemDemo = Omit<CartItem, "_id">;
